d230507d608bec54aeeea467a565ec71004f297c
Mastodont <specttre@abv.bg>
2020-12-23 06:55:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+n = int(input())
+# sum_1 = 0 right way to do the for loop
+for x in range(n):
+    x = int(input())
+    # sum_1 += x  otherwsie wont take the las x into the calculation
+    x += x # not gonna work properly 
+print(float(x / n)) # it is going to divide 10 / 3 instead of 15 / 3 
