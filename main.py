a48196fc657c6627e2b18a7fa7410d1c79d87aef
Mastodont <specttre@abv.bg>
2020-12-23 08:00:23 +0000

    new

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ for x in range(n):
     x = int(input())
     # sum_1 += x  otherwsie wont take the las x into the calculation
     x += x # not gonna work properly 
-print(float(x / n)) # it is going to divide 10 / 3 instead of 15 / 3 
+print(float(x / n)) # it is going to divide 10 / 3 instead of 15 / 3 >>>> range(n),range(n+1) etc.
